d7195aea361cc91c341c7d0d0764728d86f8aeda
popo725 <tlqlang2000@gmail.com>
2026-08-20 03:10:44 +0800

    refactor: make existing bazi scripts importable

---
+++ scripts/__init__.py
@@ -0,0 +1,5 @@
+"""Legacy deterministic fortune-calculation scripts.
+
+Commercial-site code should access these modules through adapters/services rather
+than importing implementation details directly from HTTP routes.
+"""
